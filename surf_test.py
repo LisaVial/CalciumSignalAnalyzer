8ab1cef7a88f7a224dea6656eb3d01251c2cf727
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-10-07 17:41:53 +0200

    testing surf even more

--- surf_test.py
+++ surf_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from PIL import Image
 import cv2
 from IPython import embed
 
@@ -9,7 +10,7 @@ def get_circular_rois_mask(img, kp, plot_on = False):
     non_overlap = []
     overlap_indices = []
     masks = []
-    h, w = img.shape
+    h, w = img.shape[0], img.shape[1]
     mask = np.zeros((h, w), dtype=bool)
     for keypoint_index in range(len(kp)):
         if keypoint_index in overlap_indices:
@@ -20,9 +21,10 @@ def get_circular_rois_mask(img, kp, plot_on = False):
             if cv2.KeyPoint_overlap(kp[keypoint_index], kp[next_keypoint_index]) > 0.0:
                 overlap_indices.append(next_keypoint_index)
 
+    # for non_overlap_index in range(len(non_overlap)):
     for non_overlap_index in range(len(non_overlap)):
         x_center, y_center = int(non_overlap[non_overlap_index].pt[0]), int(non_overlap[non_overlap_index].pt[1])
-        radius = 2    # radius
+        radius = 10    # radius
         mask_mask = np.array([])
         for x_offset in range(-radius, radius):
             for y_offset in [0, radius/2, 0, -(radius/2), radius, radius/2, 0, -(radius/2), radius, radius/2, 0,
@@ -45,54 +47,59 @@ def get_circular_rois_mask(img, kp, plot_on = False):
     return mask
 
 
-root = r'G:\Lisa\data\Niko'
-surf = cv2.xfeatures2d.SURF_create(800)
+root = r'G:\Lisa\data\Niko\tiff_stack.png'
+surf = cv2.xfeatures2d.SURF_create(hessianThreshold=16000, nOctaves=8)
 fig, axs = plt.subplots(1, 1)
-for root, sub_folders, files in os.walk(root):
-    for file in files:
-        kps = []
-        intensities = []
-        new_root = root + '\\'
-        tiff_stack = cv2.imreadmulti(os.path.join(new_root + file))
-        for i in range(len(tiff_stack[1])):
-            # outfile = os.path.splitext(file)[0] + '_' + str(i) + '.png'
-            # plt.imshow(tiff_stack[i])
-            # ax = plt.gca()
-            # ax.get_xaxis().set_visible(False)
-            # ax.get_yaxis().set_visible(False)
-            # ax.get_xaxis().set_ticks([])
-            # ax.axes.get_yaxis().set_ticks([])
-            # plt.imsave(new_root + outfile, tiff_stack[i], cmap='gray')
-            img = tiff_stack[1][i]
-            if i == 0:
-                plt.imshow(img)
-                plt.show()
-            # img.thumbnail(img.size)
-            # img.convert('RGB')
-            # img.save(new_root + outfile, "JPEG", quality=100)
-            kp, des = surf.detectAndCompute(img, None)
-            kps += kp
-            # embed()
-            rois = get_circular_rois_mask(img, kps, plot_on=False)
+# for root, sub_folders, files in os.walk(root):
+#     for file in files:
+#         if file == 'Cortex 10x 20fps gain 2_MMStack_Default.tif':
+kps = []
+intensities = []
+new_root = root + '\\'
+# tiff_stack = cv2.imread(os.path.join(new_root + file), flags=cv2.IMREAD_GRAYSCALE)
+# tiff_stack = cv2.imreadmulti(os.path.join(new_root + file), flags=(cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH))
+# tiff_stack = cv2.imreadmulti(root, flags=(cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH))
+# embed()
+# for i in range(len(tiff_stack[1])):
+#     print(i)
+#     # outfile = os.path.splitext(file)[0] + '_' + str(i) + '.png'
+#     # plt.imshow(tiff_stack[i])
+#     # ax = plt.gca()
+#     # ax.get_xaxis().set_visible(False)
+#     # ax.get_yaxis().set_visible(False)
+#     # ax.get_xaxis().set_ticks([])
+#     # ax.axes.get_yaxis().set_ticks([])
+#     # plt.imsave(new_root + outfile, tiff_stack[i], cmap='gray')
+#     img = tiff_stack[1][i]
+img = cv2.imread(root)
+# img.thumbnail(img.size)
+# img.convert('RGB')
+# img.save(new_root + outfile, "JPEG", quality=100)
+# change that.... maybe collect kps and only get circulat rois mask at the end of the loop
+kp, des = surf.detectAndCompute(img, None)
+# embed()
+print('%i rois detected, now drawing them' % len(kp))
+rois = get_circular_rois_mask(img, kp)
 
-            h, w = rois.shape
-            roi_image = np.zeros((h, w, 3), np.uint8)
+h, w = rois.shape
+roi_image = img
 
-            for x in range(h-1):
-                for y in range(w-1):
-                    if rois[x][y]:
-                        roi_image[x][y] = img[x][y]
+for x in range(h-1):
+    for y in range(w-1):
+        if rois[x][y]:
+            roi_image[x][y] = img[x][y]
 
-            outfile_2 = os.path.splitext(file)[0] + '_surf_' + str(i) + '.png'
-            if i == tiff_stack.shape[0]:
-                plt.imshow(roi_image)
-                plt.show()
-                # for x_idx, y_idx in zip(np.where(rois==True)[0], np.where(rois==True)[2]):
-                #     embed()
-                #     intensities = img[rois]
-            # img_surf = cv2.drawKeypoints(img, kps, None, (255, 0, 0), 2)
-            # axs.imshow(img_surf)
-            # plt.imsave(new_root + outfile, tiff_stack[i], cmap='gray')
-            # plt.show()
+# outfile_2 = os.path.splitext(root)[0] + '_surf_' + str(i) + '.png'
+
+plt.imshow(img)
+plt.imshow(roi_image)
+plt.show()
+                    # for x_idx, y_idx in zip(np.where(rois==True)[0], np.where(rois==True)[2]):
+                    #     embed()
+                    #     intensities = img[rois]
+                # img_surf = cv2.drawKeypoints(img, kps, None, (255, 0, 0), 2)
+                # axs.imshow(img_surf)
+                # plt.imsave(new_root + outfile, tiff_stack[i], cmap='gray')
+                # plt.show()
 
 # img = cv2.imread(tif_path, cv2.IMREAD_GRAYSCALE)
